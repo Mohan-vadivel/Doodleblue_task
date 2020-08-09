5a89c5e9b046b5e314d196e22387a4e02275ffef
Mohan-vadivel <37893595+Mohan-vadivel@users.noreply.github.com>
2020-08-09 20:51:12 +0530

    Add files via upload

---
+++ Doodleblue/assets/js/custom.js
@@ -0,0 +1,136 @@
+(function ($) {
+
+	"use strict";
+
+
+	$('.feature-slider').owlCarousel({
+		loop: true,
+		margin: 30,
+		nav: true,
+		pagination: false,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1
+			},
+			1000: {
+				items: 2
+			}
+		}
+	});
+	$('.case_slider').owlCarousel({
+		loop: true,
+		margin: 30,
+		nav: false,
+		pagination: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 2
+			},
+			1000: {
+				items: 3
+			}
+		}
+	});
+	$('.testimonial_slider').owlCarousel({
+		loop: true,
+		margin: 30,
+		nav: false,
+		pagination: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1
+			},
+			1000: {
+				items: 1
+			}
+		}
+	});
+
+	/*$('.owl-carousel').owlCarousel({
+		loop: true,
+		margin: 30,
+		nav: true,
+		pagination: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1
+			},
+			1000: {
+				items: 2
+			}
+		}
+	})*/
+
+
+	$(window).scroll(function () {
+		var scroll = $(window).scrollTop();
+		var box = $('.banner').height();
+		var header = $('header').height();
+
+		if (scroll >= box - header) {
+			$("header").addClass("background-header");
+		} else {
+			$("header").removeClass("background-header");
+		}
+	});
+
+	// Window Resize Mobile Menu Fix
+	mobileNav();
+
+
+	// Scroll animation init
+	window.sr = new scrollReveal();
+
+
+	// Menu Dropdown Toggle
+	if ($('.menu-trigger').length) {
+		$(".menu-trigger").on('click', function () {
+			$(this).toggleClass('active');
+			$('.header-area .nav').slideToggle(200);
+		});
+	}
+
+
+	// Menu elevator animation
+	$('a[href*=\\#]:not([href=\\#])').on('click', function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var targetHash = this.hash;
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				var width = $(window).width();
+				if (width < 991) {
+					$('.menu-trigger').removeClass('active');
+					$('.header-area .nav').slideUp(200);
+				}
+				$('html,body').animate({
+					scrollTop: (target.offset().top)
+				}, 700, 'swing', function () {
+					window.location.hash = targetHash;
+				});
+				return false;
+			}
+		}
+	});
+
+
+	// Window Resize Mobile Menu Fix
+	$(window).on('resize', function () {
+		mobileNav();
+	});
+
+
+
+})(window.jQuery);
